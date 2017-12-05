f2d19f77c4e05d8b8a2a50b6630e28f306f6ba91
bryan <bryan.sandras@hotmail.fr>
2017-12-05 23:44:46 +0100

    ajout src tpsdes

---
+++ TPSDES/src/Convert.java
@@ -0,0 +1,21 @@
+import java.util.Vector;
+
+public class Convert {
+	public static String charToBinaryString(char c) {
+		StringBuilder res = new StringBuilder();
+		String str = Integer.toBinaryString(c);
+		for(int i=0; i<8-str.length(); i++) {
+			res.append("0");
+		}
+		res.append(str);
+		return res.toString();
+	}
+	
+	public static Vector<Integer> stringToVectorInteger(String str) {
+		Vector<Integer> res = new Vector<Integer>();
+		for (int i=0; i < str.length(); i++) {
+			res.addElement(Character.getNumericValue(str.charAt(i)));
+		}
+		return res;
+	}
+}
